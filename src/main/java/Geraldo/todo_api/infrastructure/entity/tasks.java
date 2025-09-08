6b6fa3c6274999115a941d7adffa33514d794b53
Geraldo Pereira <geraldo.pereirads@hotmail.com>
2025-09-08 20:25:18 -0300

    feat(entity): add initial project setup and Tarefa entity structure

---
+++ src/main/java/Geraldo/todo_api/infrastructure/entity/tasks.java
@@ -0,0 +1,31 @@
+package Geraldo.todo_api.infrastructure.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tasks")
+@Entity
+public class tasks {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "deliveryDate")
+    private LocalDate deliveryDate;
+
+    @Column(name = "responsible")
+    private String responsible;
+
+}
